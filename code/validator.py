8a727a398c8ddbd95a7f9cef01a95291718f8023
Parth Iyer <parth.iyer@gmail.com>
2025-04-17 18:06:46 -0400

    A wrapped validator

--- code/validator.py
+++ code/validator.py
@@ -1,4 +1,4 @@
-import re
+import argparse, re
 
 class Vertex:
     def __init__(self, id : int, x : int, y : int):
@@ -24,6 +24,9 @@ class Car:
         self.src = src
         self.dest = dest
         self.path = []
+    
+    def __repr__(self):
+        return f'({self.src},{self.dest})'
 
 
 def get_edge(src: list[Edge], dest : int) -> Edge:
@@ -32,101 +35,130 @@ def get_edge(src: list[Edge], dest : int) -> Edge:
             return edge
     return None
 
-def get_cost(e : Edge):
+def get_cost(e : Edge, vertices : list[Vertex]):
     start = vertices[e.start]
     end = vertices[e.end]
     return abs(end.x - start.x) + abs(end.y - start.y) 
 
 
-graphFile = open("valid.test", "r").read().split("\n")
+def parse_problem(file_name : str):
+    graphFile = open(file_name, "r").read().split("\n")
+
+    vertices = []
+    edges = []
+    cars = []
+
+    # Read Data from files
+    e = 0
+    for i in range(len(graphFile)):
+        if graphFile[i] == "EDGES":
+            e = 1
+            continue
+        elif graphFile[i] == "CARS":
+            e = 2
+            continue
+
+        if e < 1:
+            v = graphFile[i].split(":")[1].split(",")
+            vertices.append(Vertex(i, int(v[0].strip("()")), int(v[1].strip("()"))))
+        elif e < 2:
+            v = re.split(r'([(),])', graphFile[i].split(":")[1])
+            v = list(filter(lambda x: x not in ['','(',')',','], v))
+            l = []
+            for i in range(0,len(v),3):
+                l.append(Edge(int(v[i].strip("()")), int(v[i+1].strip("()")), int(v[i+2].strip("()"))))
+            edges.append(l)
+        else:
+            v = graphFile[i].split(",")
+            cars.append(Car(int(v[0].strip("()")), int(v[1].strip("()"))))
+    
+    return vertices, edges, cars
 
 
-vertices = []
-edges = []
-cars = []
+def parse_solution(file_name : str, cars : list[Car]):
+    solnFile = open(file_name, "r").read().split("\n")
+    
+    for i in range(len(solnFile)):
+        cars[i].path = list(map(int,solnFile[i].split(":")[1].split(",")))
 
-# Read Data from files
-e = 0
-for i in range(len(graphFile)):
-    if graphFile[i] == "EDGES":
-        e = 1
-        continue
-    elif graphFile[i] == "CARS":
-        e = 2
-        continue
 
-    if e < 1:
-        v = graphFile[i].split(":")[1].split(",")
-        vertices.append(Vertex(i, int(v[0].strip("()")), int(v[1].strip("()"))))
-    elif e < 2:
-        v = re.split(r'([(),])', graphFile[i].split(":")[1])
-        v = list(filter(lambda x: x not in ['','(',')',','], v))
-        l = []
-        for i in range(0,len(v),3):
-            l.append(Edge(int(v[i].strip("()")), int(v[i+1].strip("()")), int(v[i+2].strip("()"))))
-        edges.append(l)
-    else:
-        v = graphFile[i].split(",")
-        cars.append(Car(int(v[0].strip("()")), int(v[1].strip("()"))))
-
-solnFile = open("valid.soln", "r").read().split("\n")
-for i in range(len(solnFile)):
-    cars[i].path = list(map(int,solnFile[i].split(":")[1].split(",")))
-
-# Validate the cars are correct and the path actually exists
-for c in cars:
-    if (c.src < 4096 and c.dest < 4096 and c.path[0] == c.src and c.path[-1] == c.dest):
-        for i in range(1,len(c.path)):
-            ok = False
-            for edge in edges[c.path[i-1]]:
-                if edge.end == c.path[i]:
-                    ok = True
-            if not ok:
-                print("Validation Failed!")
-                break    
-    else:
-        print("Validation Failed!")
+def validate_solution(cars : list[Car], edges : list[list[Edge]]):
+    # Validate the cars are correct and the path actually exists
+    for c in cars:
+        if (c.src < 4096 and c.dest < 4096 and c.path[0] == c.src and c.path[-1] == c.dest):
+            for i in range(1,len(c.path)):
+                ok = False
+                for edge in edges[c.path[i-1]]:
+                    if edge.end == c.path[i]:
+                        ok = True
+                if not ok:
+                    print("Validation Failed!")
+                    break    
+        else:
+            print("Validation Failed!")
 
 # Simulation
-
-# Initialize the simulation
-locs = []
-cursor = []
-waiting = []
-costs = []
-for i in range(len(cars)):
-    locs.append(cars[i].path.pop(0))
-    cursor.append(Edge(-1,-1,-1))
-    waiting.append(0)
-    costs.append(0)
-
-time = 0
-while locs.count(-1) < len(locs):
+def simulate(vertices : list[Vertex], edges : list[list[Edge]], cars : list[Car]):
+    # Initialize the simulation
+    locs = []
+    cursor = []
+    waiting = []
+    costs = []
     for i in range(len(cars)):
-        if waiting[i] == 0:
-            if len(cars[i].path) == 0:
-                cursor[i].load -= 1
-                waiting[i] = -1
-                locs[i] = -1
-                cursor[i] = None
-                continue
+        locs.append(cars[i].path.pop(0))
+        cursor.append(Edge(-1,-1,-1))
+        waiting.append(0)
+        costs.append(0)
 
-            next_edge = get_edge(edges[locs[i]], cars[i].path[0])
-            if (next_edge.load == next_edge.capacity):
-                waiting[i] = 1
-            else:
-                cursor[i].load -= 1
-                next_edge.load += 1
-                waiting[i] = get_cost(next_edge)
-                costs[i] += waiting[i]
-                locs[i] = next_edge.end
-                cursor[i] = next_edge
-                cars[i].path.pop(0)
+    # Run the simulation
+    while locs.count(-1) < len(locs):
+        for i in range(len(cars)):
+            if waiting[i] == 0:
+                if len(cars[i].path) == 0:
+                    cursor[i].load -= 1
+                    waiting[i] = -1
+                    locs[i] = -1
+                    cursor[i] = None
+                    continue
 
+                next_edge = get_edge(edges[locs[i]], cars[i].path[0])
+                if (next_edge.load == next_edge.capacity):
+                    waiting[i] = 1
+                else:
+                    cursor[i].load -= 1
+                    next_edge.load += 1
+                    waiting[i] = get_cost(next_edge, vertices)
+                    costs[i] += waiting[i]
+                    locs[i] = next_edge.end
+                    cursor[i] = next_edge
+                    cars[i].path.pop(0)
+
+            
+            waiting[i] -= 1
+
+    return costs
+            
+
+def main ():
+    parser = argparse.ArgumentParser("A solution verification and simulation program")
+    parser.add_argument('problem', type=str, help='The input file for the program')
+    parser.add_argument('solution', type=str, help='The solution file for the program')
+    parser.add_argument('-s', '--simulate', action='store_true', help='Run simulation')
+
+    args = parser.parse_args()
+    vertices, edges, cars = parse_problem(args.problem)
+    parse_solution(args.solution)
+    if not validate_solution(cars, edges):
+        print("Validation Failed!")
+        return
+    
+    elif args.simulate:
+        costs = simulate(vertices, edges, cars)
+        for i in range(len(cars)):
+            print(repr(cars[i]) + ': ' + costs[i])
+
+    
+
+if __name__ == "__main__":
+    main()
         
-        waiting[i] -= 1
-
-print(costs)
-            
-
-            
